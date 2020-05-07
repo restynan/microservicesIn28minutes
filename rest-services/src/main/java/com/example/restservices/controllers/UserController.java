e977de980072f450b3a54fc8da45262a5b46bf69
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-05-07 16:46:20 -0500

    hateOas

--- rest-services/src/main/java/com/example/restservices/controllers/UserController.java
+++ rest-services/src/main/java/com/example/restservices/controllers/UserController.java
@@ -4,10 +4,15 @@ import com.example.restservices.exception.UserNotFoundException;
 import com.example.restservices.models.User;
 import com.example.restservices.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -23,23 +28,44 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
      User user= userService.findOne(id);
      if (user==null){
          throw new UserNotFoundException("id- "+id);
      }
-return user;
+
+     //using heros to add link for retrieve all users link
+        EntityModel<User> model = new EntityModel<>(user);
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
+
 
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = userService.save(user);
+        //returning response 201 and providing a link in the headers for the newly created user
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void  deleteUser(@PathVariable int id) {
+        User user= userService.deleteById(id);
+        if (user==null){
+            throw new UserNotFoundException("id- "+id);
+        }
+
 
     }
 }
--- rest-services/src/main/java/com/example/restservices/service/UserService.java
+++ rest-services/src/main/java/com/example/restservices/service/UserService.java
@@ -5,37 +5,56 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+
 @Service
 public class UserService {
-    private static List<User> users=new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
 
-    private static int count =3;
-    static{
-        users.add(new User(1,"Resty",new Date()));
-        users.add(new User(2,"Mike",new Date()));
-        users.add(new User(3,"John",new Date()));
+    private static int count = 3;
 
-    }
-    public List<User> findAll(){
-                   return users;
+    static {
+        users.add(new User(1, "Resty", new Date()));
+        users.add(new User(2, "Mike", new Date()));
+        users.add(new User(3, "John", new Date()));
 
     }
-    public User findOne(int id){
-        for(User user:users){
-            if(user.getId()==id){
+
+    public List<User> findAll() {
+        return users;
+
+    }
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
                 return user;
             }
         }
-          return null;
+        return null;
 
     }
-    public User save(User user){
-        if( user.getId()==null){
+
+    public User save(User user) {
+        if (user.getId() == null) {
             user.setId(count++);
-       }
+        }
         users.add(user);
         return user;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+            if (user.getId() == id) {
+                iterator.remove();
+                return user;
+            }
+        }
+        return null;
+
     }
 
 }
